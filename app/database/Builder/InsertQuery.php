a3de15c1d228b7880b26f2ebb2a85b520efd096a
LenizaOMD2009 <lenizaomnd@gmail.com>
2026-04-30 22:48:00 -0300

    a

--- app/database/Builder/InsertQuery.php
+++ app/database/Builder/InsertQuery.php
@@ -8,25 +8,42 @@ class InsertQuery
 {
     private string $table;
     private array $FieldsAndValues = [];
-    #Obter o nome da tabela onde os dados serão inserido.
+
     public static function table(string $table): self
     {
         $self = new self;
         $self->table = $table;
         return $self;
     }
+
+    public static function insert(string $table): self
+    {
+        return self::table($table);
+    }
+
     private function createQuery(): string
     {
-        $fields  = implode(',', array_keys($this->FieldsAndValues));
+        $fields      = implode(',', array_keys($this->FieldsAndValues));
         $placeHolder = ':' . implode(',:', array_keys($this->FieldsAndValues));
-        return "insert into $this->table ($fields) values ($placeHolder);";
+        return "INSERT INTO {$this->table} ({$fields}) VALUES ({$placeHolder});";
     }
+
     private function execute(string $query): bool
     {
-        $con = Connection::connection();
+        $con     = Connection::connection();
         $prepare = $con->prepare($query);
-        return $prepare->execute($this->FieldsAndValues);
+        foreach ($this->FieldsAndValues as $key => $value) {
+            if (is_bool($value)) {
+                $prepare->bindValue(':' . $key, $value, \PDO::PARAM_BOOL);
+            } elseif (is_null($value)) {
+                $prepare->bindValue(':' . $key, $value, \PDO::PARAM_NULL);
+            } else {
+                $prepare->bindValue(':' . $key, $value, \PDO::PARAM_STR);
+            }
+        }
+        return $prepare->execute();
     }
+
     public function save(array $FieldsAndValues): bool
     {
         $this->FieldsAndValues = $FieldsAndValues;
